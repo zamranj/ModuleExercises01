de23286c49b591eb4cfa19111f87248377a40558
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-24 19:34:53 +0000

    Initial commit

---
+++ randomSelect.js
@@ -0,0 +1,11 @@
+function randomFromArray(arr){
+  //Your code here to select a random element from the array passed to the function.
+  let index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+}
+
+//TODO: Export the randomFromArray function.
+
+module.exports ={
+  randomFromArray: randomFromArray
+};
